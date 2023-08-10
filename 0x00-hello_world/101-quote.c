34e8de344f7b64399efefc2914dc903d1c5985d0
julioceaseless <julioceaseless@gmail.com>
2023-08-10 14:12:44 -0700

    betty betty proud betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 #define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
 /**
   * main - Program entry point
-  * print the size of various types
+  *
+  * prints message to the standard error
+  * and returns an exit status of 1
   *
   * Return: Always 1  (Exit status)
   */
-
-/* main goes here */
 int main(void)
 {
 	char msg[] = MESSAGE;
