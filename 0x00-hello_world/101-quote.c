e9021023af5e19862a1f290cf3670d237f2a6786
julioceaseless <julioceaseless@gmail.com>
2023-08-10 14:05:51 -0700

    betty betty proud betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
 #define MESSAGE "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
-/*
- * main - Entry point
- *
- * This function is the entry point of the program.
- * It prints a specific message to the standard error
- * and returns an exit status of 1.
- *
- * Return: Always 1 (Exit status)
-*/
+/**
+  * main - Program entry point
+  * print the size of various types
+  *
+  * Return: Always 1  (Exit status)
+  */
+
+/* main goes here */
 int main(void)
 {
 	char msg[] = MESSAGE;
