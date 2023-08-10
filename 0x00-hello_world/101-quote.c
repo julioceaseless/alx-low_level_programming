0977a0acb3a71da26caeb5fd65b9c802682f44cf
julioceaseless <julioceaseless@gmail.com>
2023-08-10 13:27:01 -0700

    print withoyt put() or printf()

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,10 +6,11 @@
  * Return: Always returns 1 (Success)
  *
  */
+#define MESSAGE "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n"
 int main(void)
 {
-        char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char msg[] = MESSAGE;
 
-        write(2, msg, sizeof(msg) - 1);
-        return (1);
+	write(2, msg, sizeof(msg) - 1);
+	return (1);
 }
