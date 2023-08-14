35272450115d5c9872fed09c7e9415214b80bcfc
julioceaseless <julioceaseless@gmail.com>
2023-08-14 04:33:00 -0700

    print 2 two-digit nums

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints 4 digits
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+int i;
+int j;
+int k;
+int l;
+
+for (i = 0; i <= 9; i++)
+{
+	for (j = 0; j <= 9; j++)
+	{
+		for (k = 0; k <= 9; k++)
+		{
+			for (l = 0; l <= 9; l++)
+			{
+				if ((k * 10 + l) > (i * 10 + j))
+				{
+					putchar (i + '0');
+					putchar (j + '0');
+					putchar (' ');
+					putchar (k + '0');
+					putchar (l + '0');
+					if (i != 9 || j != 8 || k != 9 || l != 9)
+					{
+						putchar (',');
+						putchar (' ');
+					}
+				}
+			}
+		}
+	}
+}
+putchar ('\n');
+return (0);
+}
