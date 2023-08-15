2d879db904a7f9d57a5402beed38a5a1be74bc61
julioceaseless <julioceaseless@gmail.com>
2023-08-15 12:09:10 +0000

    main header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <stdlib.h>
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int);
+int print_sign(int);
+int print_last_digit(int);
+int add(int, int);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+void jack_bauer(void);
+void times_table(void);
+#endif
