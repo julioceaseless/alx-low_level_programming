6d459815cb929de29fcc4a4f7e6a498315307488
julioceaseless <julioceaseless@gmail.com>
2023-08-15 18:03:10 +0000

    double fib

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,17 @@
   */
 int main(void)
 {
-	double num1 = 1;
-	double num2 = 2;
-	double sum;
+	int num1 = 1;
+	int num2 = 2;
+	int sum;
 	int i;
 
-	printf("%0.lf, %0.lf", num1, num2);
+	printf("%d, %d", num1, num2);
 
 	for (i = 2; i < 96; i++)
 	{
 		sum = num1 + num2;
-		printf(", %0.lf", sum);
+		printf(", %d", sum);
 
 		num1 = num2;
 		num2 = sum;
