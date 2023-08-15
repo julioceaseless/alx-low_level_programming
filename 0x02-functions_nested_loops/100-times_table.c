b4a2beb295784cd7aabd8c407f637391f2dd1c29
julioceaseless <julioceaseless@gmail.com>
2023-08-15 15:05:59 +0000

    updated

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n*n table
+ *
+ * @n: size of table
+ * Return: returns last digit
+ */
+
+void print_times_table(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if (i * j > 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (i * j) / 100);
+					_putchar('0' + ((i * j) / 10) % 10);
+					_putchar('0' + (i * j) % 10);
+				}
+				else if (i * j > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (i * j) / 10);
+					_putchar('0' + (i + j) % 10);
+				}
+				else
+				{
+					if (j != 0)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar('0' + i * j);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -17,4 +17,5 @@ void print_alphabet_x10(void);
 void jack_bauer(void);
 void times_table(void);
 void print_to_98(int n);
+void print_times_table(int n);
 #endif
