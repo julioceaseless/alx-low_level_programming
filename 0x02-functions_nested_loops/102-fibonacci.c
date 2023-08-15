cdc08ed130005011d9fd8952cbfbe4e589d2067c
julioceaseless <julioceaseless@gmail.com>
2023-08-15 16:19:39 +0000

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the first 50 fibonacci numbers
+  *
+  * Return: Always 0!
+  */
+int main(void)
+{
+	long num1 = 1;
+	long num2 = 2;
+	int i = 0;
+
+	printf("%ld, ", num1);
+	printf("%ld", num2);
+
+	while (i < 49)
+	{
+		num2 += num1;
+		num1 = num2 - num1;
+
+		printf(", %ld", num2);
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
+
