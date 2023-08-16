791683491358b3f06c181ea156c4c0f219580430
julioceaseless <julioceaseless@gmail.com>
2023-08-16 15:41:03 +0000

    fibo

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,15 +9,15 @@ int main(void)
 	unsigned long num2 = 2;
 	unsigned long num1_part1, num1_part2, num2_part1, num2_part2;
 	unsigned long sum_part1, sum_part2;
-	unsigned long div =  10000000000;
+	unsigned long div =  1000000000;
 	int i;
 
 	printf("%lu, %lu", num1, num2);
 	for (i = 3; i < 93; i++)
 	{
-		printf(", %lu", num2);
 		num2 = num1 + num2;
 		num1 = num2 - num1;
+		printf(", %lu", num2);
 	}
 	/** num1 **/
 	num1_part1 = num1 / div;
@@ -26,8 +26,9 @@ int main(void)
 	/**  num2 **/
 	num2_part1 = num2 / div;
 	num2_part2 = num2 % div;
-
-	for (i = 93; i < 99; i++)
+	
+	i = 93;
+	while (i < 99)
 	{
 		sum_part2 = (num1_part2 + num2_part2) % div;
 		sum_part1 = num1_part1 + num2_part1;
@@ -39,6 +40,7 @@ int main(void)
 		num2_part2 += num1_part2;
 		num1_part1 = num2_part1 - num1_part1;
 		num1_part2 = num2_part2 - num1_part2;
+		i++;
 	}
 	printf("\n");
 	return (0);
