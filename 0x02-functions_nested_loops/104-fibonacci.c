5d6b5b7280393e8e1ab65afdbc0f53e518c1e8f1
julioceaseless <julioceaseless@gmail.com>
2023-08-16 15:28:22 +0000

    fibo

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 	int i;
 
 	printf("%lu, %lu", num1, num2);
-	for (i = 3; i < 92; i++)
+	for (i = 3; i < 93; i++)
 	{
 		printf(", %lu", num2);
 		num2 = num1 + num2;
@@ -27,7 +27,7 @@ int main(void)
 	num2_part1 = num2 / div;
 	num2_part2 = num2 % div;
 
-	for (i = 92; i < 99; i++)
+	for (i = 93; i < 99; i++)
 	{
 		sum_part2 = (num1_part2 + num2_part2) % div;
 		sum_part1 = num1_part1 + num2_part1;
