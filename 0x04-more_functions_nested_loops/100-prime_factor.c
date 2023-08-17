bb79acf3ff6152ff56bbd2a13e46eb93f9db4976
julioceaseless <julioceaseless@gmail.com>
2023-08-17 17:28:39 +0000

    prime factors

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,24 +8,40 @@
  */
 int main(void)
 {
-	long num = 612852475143;
-	long l_prime = 3;
+	unsigned long num = 612852475143;
+	unsigned long l_prime = 2;
 
-	if (num % 3 == 0)
+	/* divide by 2 until it is odd */
+	while (num % l_prime == 0)
 	{
-		while (num % 3 == 0)
-		{
-			num /= 3;
-
-		}
-
+		num /= l_prime;
 	}
+
+	/* divide by 3 */
+	if (num % (++l_prime) == 0)
+	{
+		while (num % l_prime == 0)
+		{
+			num /= l_prime;
+		}
+	}
+
+	l_prime += 2;
+
+	/* find the next divider */
 	while (num % l_prime != 0)
 	{
 		l_prime += 2;
-
 	}
-	printf(" %lu\n", l_prime);
+	num /= l_prime;
 
+	if (l_prime > num)
+	{
+		printf("%lu\n", l_prime);
+	}
+	else
+	{
+		printf("%lu\n", num);
+	}
 	return (0);
 }
