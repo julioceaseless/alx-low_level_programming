4b1e46f3956be3bcdb6c5138862fb0df8a839073
julioceaseless <julioceaseless@gmail.com>
2023-08-22 17:04:52 +0000

    crack me

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main entry
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	const int checksum = 2772;
+	int pass[100];
+	int i;
+	int sum = 0;
+	int n = 0;
+
+	/* seed the random number generated to current time */
+	srand(time(NULL));
+
+	/* generated 100 random numbers between 0 - 77 */
+	for (i = 0; i < 100; i++)
+	{
+		pass[i] = (rand() % 78);
+
+		sum += pass[i];
+
+		/* print ASCII equivalent of each num */
+		putchar(pass[i] + 48);
+
+	}
+
+	if ((checksum - sum) < 78)
+	{
+		n = checksum - sum;
+		sum += n;
+		putchar(n + 48);
+	}
+
+	return (0);
+}
