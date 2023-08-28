2d1860202bb119f38b1123a08a4844391f8d148b
julioceaseless <julioceaseless@gmail.com>
2023-08-28 11:36:11 +0000

    pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * set_string - copies the characters in string pointed to by to
+ * to string pointed to by *s
+ * @s: pointer to a pointer to string
+ * @to: pointer to string
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		(*s)[i] = to[i];
+		i++;
+	}
+	(*s)[i] = '\0';
+}
