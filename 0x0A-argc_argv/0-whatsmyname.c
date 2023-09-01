8a02f66ad86dd361d1bd449223c3584ef10084c2
julioceaseless <julioceaseless@gmail.com>
2023-09-01 10:14:28 +0000

    betty compliant

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
- *
- *
+ * main - main program entry
+ * @argc: keeps the count of the number of command line arguments
+ * @argv: pointer to an array of command line arguments
+ * Return: 0 for success
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc-1]);
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
