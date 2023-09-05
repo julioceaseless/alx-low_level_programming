0052f5871078c8667b81874c525061ec8126715a
julioceaseless <julioceaseless@gmail.com>
2023-09-05 01:17:41 +0000

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,19 +26,19 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2, i;
 	char *joined_str;
 
-	if (s2 == NULL && s1 == NULL)
-		return (NULL);
-
 	if (s2 == NULL)
-		return (s1);
+		s2 = "";
 
 	if (s1 == NULL)
-		return (s2);
+		s1 = "";
 
 	len1 = str_len(s1);
 	len2 = str_len(s2);
 
 	joined_str = (char *) malloc((len1 + len2 + 1) * sizeof(char));
+
+	if(joined_str == NULL)
+		return (NULL);
 
 	for (i = 0; i < len1; i++)
 	{
