c3c7055cafec240c8754d95266742da284350090
julioceaseless <julioceaseless@gmail.com>
2023-09-07 15:00:35 +0000

    recreate realloc()

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * _realloc - resizes previously allocated memory block.
+ * mimics realloc() function
+ * @ptr: pointer to old memory block
+ * @old_size: old size of memory
+ * @new_size: new size of memory
+ * Return: pointer to new memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr = NULL;
+	unsigned int i, mem_size = 0;
+
+	ptr = NULL;
+
+	ptr = malloc(old_size);
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (ptr != NULL && new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (new_size == old_size)
+		return (ptr);
+
+	/* reallocate memory */
+	new_ptr = malloc(new_size);
+
+	if (new_ptr == NULL)
+		return (NULL);
+	/* determine size of memory bytes to copy */
+	mem_size = (new_size > old_size) ? old_size : new_size;
+
+	/* copy content from old to new */
+	for (i = 0; i < mem_size; i++)
+	{
+		*((char *)new_ptr + i) = *((char *)ptr + i);
+	}
+	/* free old memory and return new memory block */
+	free(ptr);
+	return (new_ptr);
+}
