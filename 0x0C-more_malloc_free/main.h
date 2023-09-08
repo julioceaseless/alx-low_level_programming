89132fae2c5736577570e5f4bf1602e5459448c3
julioceaseless <julioceaseless@gmail.com>
2023-09-08 02:57:53 +0000

    updated main

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,4 +8,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
+int str_len(char *s);
+void result_to_str(int *int_arr, char *char_arr, int size);
+char *mul(char *num1, char *num2);
+void _print(char *s);
+
 #endif
