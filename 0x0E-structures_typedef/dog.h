7d85eaf55790c07ccf603e175314bf2955088060
julioceaseless <julioceaseless@gmail.com>
2023-09-11 18:07:19 +0000

    first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,21 @@
 #ifndef DOH_H
 #define DOG_H
+#include <stdlib.h>
 
+/**
+ * struct dog - struct for dog details
+ * @name: pointer to dog's name
+ * @age: dog's age
+ * @owner: pointer to owner's name
+ */
 struct dog{
 char *name;
 float age;
 char *owner;
 };
 
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
