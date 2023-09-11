5b03aed5f101a822fa8ae01d8717f4273495fb8d
julioceaseless <julioceaseless@gmail.com>
2023-09-11 18:10:44 +0000

    fix dog and betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,10 +8,11 @@
  * @age: dog's age
  * @owner: pointer to owner's name
  */
-struct dog{
-char *name;
-float age;
-char *owner;
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 typedef struct dog dog_t;
