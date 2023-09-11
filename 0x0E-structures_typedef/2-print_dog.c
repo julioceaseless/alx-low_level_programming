a53ddea4843e0fa48b71916048ed5cf48f6b5d6d
julioceaseless <julioceaseless@gmail.com>
2023-09-11 17:58:05 +0000

    first commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,15 +9,15 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return;
 
-	if (d->name == NULL)
-		d->name = "(nil)";
-	if (d->age == NULL)
-		d->age = "(nil)";
-	if (d->owner == NULL)
-		d->owner = "(nil)";
+	if ((*d).name == NULL)
+		(*d).name = "(nil)";
+	if ((*d).age == NULL)
+		(*d).age = "(nil)";
+	if ((*d).owner == NULL)
+		(*d).owner = "(nil)";
 
-	printf("Name:%s\n", d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner);
+	printf("Name:%s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner);
 
 }
