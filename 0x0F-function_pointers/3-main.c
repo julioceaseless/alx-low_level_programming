f961218a862a2f1f30a1e2437afca2ed4f440b22
julioceaseless <julioceaseless@gmail.com>
2023-09-13 18:52:17 +0000

    operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "calc.h"
+/**
+ * main - program entry
+ * @argc: number of command line arguments
+ * @argv: array of command line arguments
+ * Return: returns result
+ */
+int main(int argc, char **argv)
+{
+	int num1, num2, result;
+	int (*operation)(int, int) = NULL;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	/* initiate function pointer to the correct operation func*/
+	operation = get_op_func(argv[2]);
+
+	/* perform the correct operation */
+	if (operation != NULL)
+	{
+		result = operation(num1, num2);
+		printf("%d\n", result);
+	}
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "calc.h"
+/**
+ * op_add - adds two numbers
+ * @a: int number 1
+ * @b: int number 2
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts two numbers
+ * @a: minuend
+ * @b: subtrahend
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers
+ * @a: number 1
+ * @b: number 2
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide two numbers
+ * @a: dividend
+ * @b: divisor
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	if (b != 0)
+		return (a / b);
+
+	printf("Error\n");
+	exit(100);
+}
+
+/**
+ * op_mod - performs modulus
+ * @a: dividend
+ * @b: divisor
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	if (b != 0)
+		return (a % b);
+	printf("Error\n");
+	exit(100);
+}
