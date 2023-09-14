8243bc51c88c847a192dd7b817052bd8b5039730
julioceaseless <julioceaseless@gmail.com>
2023-09-14 14:32:49 +0000

    reduce number of variables

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -27,7 +27,10 @@ void print_all(const char * const format, ...)
 			case 's':
 				s = va_arg(args, char *);
 				if (s == NULL)
+				{
 					printf("(nil)");
+					break;
+				}
 				else
 					printf("%s", s);
 				break;
