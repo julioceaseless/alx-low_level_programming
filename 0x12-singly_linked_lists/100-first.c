80bc67634500aa4d3a8aedbc950d1d0c31979352
julioceaseless <julioceaseless@gmail.com>
2023-09-22 01:33:25 +0000

    constructor directive fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,6 +9,6 @@ void __attribute__ ((constuctor)) before_main(void);
 
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
 }
