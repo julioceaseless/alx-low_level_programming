7a75d579f58170bf0149396078cbc530eb8e53d4
julioceaseless <julioceaseless@gmail.com>
2023-09-26 13:25:26 +0000

    minor fix

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 	const listint_t *temp;
 
 	if (h == NULL)
-		return (0);
+		return (NULL);
 	temp = h;
 
 	/* traverse the linked list */
-	while (temp != 0)
+	while (temp != NULL)
 	{
 		printf("%d\n", temp->n);
 		temp = temp->next;
