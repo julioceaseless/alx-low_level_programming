8bb4efd86a2b6eb84eaf34cbaf2d46d90a2f8bc1
julioceaseless <julioceaseless@gmail.com>
2023-10-02 23:47:44 +0000

    fix formating

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,9 +3,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#define BUFFER_SIZE 1024
+
 /* prototypes */
 void open_error(int exit_code, char *message, char *filename);
 void close_error(int exit_code, char *message, int fd_value);
+
 /**
  * open_error - prints error message when opening file fails
  * @exit_code: return code
@@ -18,6 +21,7 @@ void open_error(int exit_code, char *message, char *filename)
 	dprintf(STDERR_FILENO, "%s %s\n", message, filename);
 	exit(exit_code);
 }
+
 /**
  * close_error - prints close error to stderr
  * @exit_code: return code
@@ -30,6 +34,7 @@ void close_error(int exit_code, char *message, int fd_value)
 	dprintf(STDERR_FILENO, "%s %d\n", message, fd_value);
 	exit(exit_code);
 }
+
 /**
  * main - main program entry.
  * @argc: argument count
@@ -46,28 +51,28 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	buffer = malloc(1024 * sizeof(char));
+	buffer = malloc(BUFFER_SIZE * sizeof(char));
 
 	/* open source file */
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from < 0)
-		open_error(98, "Error: Can't read from file ", argv[1]);
+		open_error(98, "Error: Can't read from file", argv[1]);
 
 	/* open dest file */
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd_to < 0)
-		open_error(99, "Error: Can't write to ", argv[2]);
+		open_error(99, "Error: Can't write to", argv[2]);
 
 	/* read from source and copy to dest file */
 	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))))
 	{
 		if (write(fd_to, buffer, bytes_read) != bytes_read)
-			open_error(99, "Error: Can't write to ", argv[2]);
+			open_error(99, "Error: Can't write to", argv[2]);
 	}
 
 	/* detect error when reading from file */
 	if (bytes_read < 0)
-		open_error(98, "Error: Can't read from file ", argv[1]);
+		open_error(98, "Error: Can't read from file", argv[1]);
 
 	f_close = close(fd_from);
 	if (f_close < 0)
