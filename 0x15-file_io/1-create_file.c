99421dcee040dd95a48a4cf61c7290915af12a92
julioceaseless <julioceaseless@gmail.com>
2023-10-02 15:57:08 +0000

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * create_file - creates a file and write text to it.
+ * @filename: name of the file
+ * @text_content: text to write to file
+ * Return: 1 (success) or -1 (failure)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd = 0, bytes = 0, i;
+
+	/* open file if exists or create it if non-existent */
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+	/* get length of string */
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	/* exit if string is empty */
+	if (i == 0)
+		return (-1);
+	/* write string to file */
+	bytes = write(fd, text_content, i);
+	if (bytes < 0)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
