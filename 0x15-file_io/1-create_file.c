99172f7961ecd939f203c28e73e9ae60bd288943
julioceaseless <julioceaseless@gmail.com>
2023-10-02 23:30:24 +0000

    updated

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,21 +9,28 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd = 0, bytes = 0, i;
 
+	if (filename == NULL)
+		return (-1);
+
 	/* open file if exists or create it if non-existent */
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
-	/* get length of string */
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	/* exit if string is empty */
-	if (i == 0)
-		return (-1);
-	/* write string to file */
-	bytes = write(fd, text_content, i);
-	if (bytes < 0)
-		return (-1);
 
+	if (*text_content != '\0')
+	{
+		/* get length of string */
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+
+		/* write string to file */
+		bytes = write(fd, text_content, i);
+		if (bytes < 0)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
 	close(fd);
 	return (1);
 }
