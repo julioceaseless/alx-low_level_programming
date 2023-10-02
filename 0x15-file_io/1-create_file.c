6b67fcb591cefa9efa27978e873c6e608b3591ac
julioceaseless <julioceaseless@gmail.com>
2023-10-02 23:34:12 +0000

    check if pointer is valid

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	if (*text_content != '\0')
+	if (text_content != NULL)
 	{
 		/* get length of string */
 		for (i = 0; text_content[i] != '\0'; i++)
