7fa079e60189c029bae3df50d3c84b457f56d4b0
julioceaseless <julioceaseless@gmail.com>
2023-10-02 23:21:35 +0000

    program copies content of file from file 1 to file 2

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/* prototypes */
+void open_error(int exit_code, char *message, char *filename);
+void close_error(int exit_code, char *message, int fd_value);
+/**
+ * open_error - prints error message when opening file fails
+ * @exit_code: return code
+ * @message: error message
+ * @filename: name of file that failed to open
+ * Return: nothing
+ */
+void open_error(int exit_code, char *message, char *filename)
+{
+	dprintf(STDERR_FILENO, "%s %s\n", message, filename);
+	exit(exit_code);
+}
+/**
+ * close_error - prints close error to stderr
+ * @exit_code: return code
+ * @message: error message
+ * @fd_value: file descriptor return value
+ * Return: nothing
+ */
+void close_error(int exit_code, char *message, int fd_value)
+{
+	dprintf(STDERR_FILENO, "%s %d\n", message, fd_value);
+	exit(exit_code);
+}
+/**
+ * main - main program entry.
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0 (success)
+ */
+int main(int argc, char **argv)
+{
+	int fd_to, fd_from, bytes_read = 0, f_close = 0;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buffer = malloc(1024 * sizeof(char));
+
+	/* open source file */
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from < 0)
+		open_error(98, "Error: Can't read from file ", argv[1]);
+
+	/* open dest file */
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (fd_to < 0)
+		open_error(99, "Error: Can't write to ", argv[2]);
+
+	/* read from source and copy to dest file */
+	while ((bytes_read = read(fd_from, buffer, sizeof(buffer))))
+	{
+		if (write(fd_to, buffer, bytes_read) != bytes_read)
+			open_error(99, "Error: Can't write to ", argv[2]);
+	}
+
+	/* detect error when reading from file */
+	if (bytes_read < 0)
+		open_error(98, "Error: Can't read from file ", argv[1]);
+
+	f_close = close(fd_from);
+	if (f_close < 0)
+		close_error(100, "Error: Can't close", fd_from);
+
+	f_close = close(fd_to);
+	if (f_close < 0)
+		close_error(100, "Error: Can't close", fd_to);
+	free(buffer);
+	return (0);
+}
