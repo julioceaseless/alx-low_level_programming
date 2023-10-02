89d872e362a8da3423871c382e9cedc1b59fc9b1
julioceaseless <julioceaseless@gmail.com>
2023-10-02 16:54:30 +0000

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,8 +8,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fdescriptor = 0;
-	char buffer[1024];
-	ssize_t total_read = 0, char_read = 0, char_written = 0;
+	char *buffer = NULL;
+	ssize_t char_read = 0, char_written = 0;
 
 	if (filename == NULL)
 		return (0);
@@ -17,33 +17,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fdescriptor = open(filename, O_RDONLY);
 	if (fdescriptor == -1)
 		return (0);
+	buffer = malloc(letters * sizeof(char));
+	if (buffer == NULL)
+		return (0);
 
-	while (letters > 0)
+	char_read = read(fdescriptor, buffer, letters);
+
+	char_written = write(STDOUT_FILENO, buffer, char_read);
+
+	if (char_written == -1 && char_written != char_read)
 	{
-		char_read = read(fdescriptor, buffer, sizeof(buffer));
-		if (char_read == -1)
-		{
-			close(fdescriptor);
-			return (0);
-		}
-		if (char_read == 0)
-			break;
-
-		char_written = write(STDOUT_FILENO, buffer, char_read);
-
-		if (char_written == -1 || char_written != char_read)
-		{
-			close(fdescriptor);
-			return (0);
-		}
-		total_read += char_read;
-		letters -= char_read;
+		close(fdescriptor);
+		return (0);
 	}
+	free(buffer);
 	close(fdescriptor);
-	return (total_read);
-
-
-
-
-
+	return (char_read);
 }
