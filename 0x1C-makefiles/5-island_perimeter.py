87db8f0b853830af26047d9bf837d6f962f48746
julioceaseless <julioceaseless@gmail.com>
2024-01-24 16:46:00 +0000

    remove island rules

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,16 +12,16 @@ def island_perimeter(grid):
             # print("[{}][{}] = {}".format(row, col, [row][col]))
             if grid[row][col]:
                 # check if island
-                if row == 0 or row == 4 or col == 0 or col == 5:
-                    return per
+                # if row == 0 or row == 4 or col == 0 or col == 5:
+                #    return per
 
                 # find island boundaries
-                if grid[row - 1][col] == 0:
+                if row == 0 or grid[row - 1][col] == 0 :
                     per += 1
-                if grid[row + 1][col] == 0:
+                if row == (len(grid) - 1) or grid[row + 1][col] == 0:
                     per += 1
-                if grid[row][col + 1] == 0:
+                if col == (len(grid[0]) - 1) or grid[row][col + 1] == 0:
                     per += 1
-                if grid[row][col - 1] == 0:
+                if col == 0 or grid[row][col - 1] == 0:
                     per += 1
     return per
