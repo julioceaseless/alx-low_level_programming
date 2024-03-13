f8a95ae2a88a6507216ce67e34aa93c40c965b4b
julioceaseless <julioceaseless@gmail.com>
2024-03-13 09:11:49 +0000

    handle null array

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -16,7 +16,10 @@ int jump_search(int *array, size_t size, int value)
 	jump_size = sqrt(size);
 	lower = 0;
 	upper = jump_size;
-
+	
+	/* handle null array */
+	if (array == NULL)
+		return (-1);
 	/* check at index 0 */
 	printf("Value checked array[%ld] = [%d]\n", lower, array[lower]);
 
